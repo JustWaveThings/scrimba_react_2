8049f8bed2a162c329da3061ec63be766f72ad5c
JustWaveThings <justwavethings@proton.me>
2023-04-19 11:38:21 -0400

    Vanilla JS example of same example

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-// Challenge: Create your own custom React component!
-// Call it "MainContent", and have it return a simple
-// h1 element that says "I'm learning React!"
-
-// Afterward, render it below the Navbar (which
-// you can do inside the ReactDOM.render call below)
-
-function MainContent() {
-	return <h1>I'm learning React</h1>;
-}
-
-ReactDOM.render(
-	<div>
-		<MainContent />
-	</div>,
-	document.getElementById('root')
-);
+const newRoot = document.getElementById('root');
+const newElement = document.createElement('h1');
+newElement.textContent = 'some text content';
+newElement.className = 'header';
+newRoot.appendChild(newElement);
